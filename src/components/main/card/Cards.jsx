4f3b242a933569a356df12a143eb8a0b81e6a9b0
LeeJinWoo <jw.lee@lulu-lab.com>
2024-01-26 16:07:17 +0900

    상세화면 작업중

--- src/components/main/card/Cards.jsx
+++ src/components/main/card/Cards.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import Card from "./Card";
 
 import CONTENT_DATAS from "../../../data";
+import { useState } from "react";
+import Detail from "../detail/detail";
 
 const StyledCards = styled.section`
   display: grid;
@@ -17,18 +19,31 @@ const StyledCards = styled.section`
 `;
 
 const Cards = () => {
+  const [detail, setDetail] = useState(false);
+
+  const handleClick = () => {
+    setDetail(true);
+  };
+
   return (
-    <StyledCards>
-      {CONTENT_DATAS.map((content) => (
-        <Card
-          key={content.title}
-          imgSrc={content.thumbnail}
-          title={content.title}
-          description={content.description}
-          period={content.period}
-        />
-      ))}
-    </StyledCards>
+    <>
+      {detail ? (
+        <Detail />
+      ) : (
+        <StyledCards>
+          {CONTENT_DATAS.map((content) => (
+            <Card
+              key={content.title}
+              imgSrc={content.thumbnail}
+              title={content.title}
+              description={content.description}
+              period={content.period}
+              onClick={handleClick}
+            />
+          ))}
+        </StyledCards>
+      )}
+    </>
   );
 };
 
